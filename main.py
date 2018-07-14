a1fd68a72299beeca3786217593871a368b78c53
Tommy Huang <tommyhuang999@gmail.com>
2018-07-14 09:52:21 -0400

    added the main loop, 60 FPS

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
 from particle import Particle
-import pygame
 from someFunctions import *
+import pygame
+from pygame import gfxdraw
+import time
+import sys
 
-bestObject = Test(input("do the thing"))
-bestObject.a = 3
-bestObject.print_double()
 
+# setup pygame
+pygame.mixer.pre_init(22050, -16, 2, 512)
+pygame.mixer.init()
+pygame.init()
+pygame.font.init()
+time.sleep(0.5)
+
+clock = pygame.time.Clock()
+disLength = 1200
+disHeight = 800
+screen = pygame.display.set_mode((disLength, disHeight))
+pygame.display.set_caption("Particle Box")
+
+# colours
+backCol = (245, 245, 245)
+
+# test stuff
+# testCircle = Particle(init_pos_x=35, init_pos_y=35, init_vel=10, init_direction=90, init_mass=300)
+# print(testCircle.calc_size())
+
+# this list holds all particle objects
+
+
+
+while True:
+    screen.fill(backCol)
+
+    # drawing aa shapes
+    # pygame.draw.circle(screen, (50, 200, 50), (testCircle.posX, testCircle.posY), int(testCircle.calc_size() + 1))
+    # pygame.gfxdraw.aacircle(screen, testCircle.posX, testCircle.posY, int(testCircle.calc_size()), (0, 0, 0))
+
+    # update display!
+    pygame.display.update()
+
+    # should make it 60FPS max
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
 
--- particle.py
+++ particle.py
@@ -1,26 +1,57 @@
 from someFunctions import *
+import math
 
 # this is simply a program that defines a particle class, used in main.py
 
 
-# needed attributes:
-# x and y velocity
-# if there is x and y velocities u dont need this
-
+# a circle!!
+# pos x and y go from top left corner to bottom right corner
+# direction is in radians
+# velocity is total velocity
+# mass is mass :D
+# damage increases after collisions, if damage goes above the dmgThreshold, the particle will break apart
+# standard particle mass is 100
+# particle heals based on its mass, damage, and a base rate
 class Particle:
-    def __init__(self, init_pos_x, init_pos_y, init_vel_x, init_vel_y):
-        self.velX = init_vel_x
-        self.velY = init_vel_y
+    # initial attributes
+    damage = 0
+    dmg_threshold = 1
+    size = 1
+
+    # initialize the object
+    def __init__(self, init_pos_x, init_pos_y, init_vel, init_direction, init_mass):
+        # these are defined on particle creation
+        self.vel = init_vel
+        self.direction = init_direction
         self.posX = init_pos_x
         self.posY = init_pos_y
-        # add later
-        # self.totalVel =
-        pass
-    pass
+        self.mass = init_mass
 
-class Test:
-    def __init__(self, val):
-        self.a = val
-    def print_double(self):
-        print(float(self.a) * 2)
+        # based on the defined attributes in __init__
+        damage = 0  # starts with minimum damage, gradually lowers to 0 if greater than 0
+        self.dmg_threshold = self.calc_threshold()  # explode if damage passes this
+        self.size = self.calc_size()  # radius of the particle
 
+    # functions of particle
+    def calc_threshold(self):
+        return self.mass
+
+    # get radius of particle
+    def calc_size(self):
+        return math.sqrt(self.mass)
+
+    # returns how much the particle heals
+    def calc_heal(self):
+        return math.sqrt((self.damage / 200) + (self.mass / 200))
+
+    def update_self(self):
+        self.dmgThreshold = self.calc_threshold()
+        self.size = self.calc_size()
+        self.damage -= self.calc_heal()
+
+
+# class Test:
+#     def __init__(self, val):
+#         self.a = val
+#     def print_double(self):
+#         print(float(self.a) * 2)
